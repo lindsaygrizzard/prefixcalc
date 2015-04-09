c411d891c117563bbbfb6e06a48e4d5b71fd74e0
Hackbright Student <info@hackbrightacademy.com>
2015-04-09 12:38:24 -0700

    working prefix calc

--- calculator.py
+++ calculator.py
@@ -5,26 +5,38 @@ Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
 
-from arithmetic1 import *
+from arithmetic import *
 
 
 # Your code goes here
-
+print "Please enter your calculations in prefix mode with spaces as separation between items."
+print "Print q to quit."
+print "Type squ to squaring your number, cube to cube, pow, mod, (+ - * / as usual)"
 
 
 def calculator():
     while True:
-        print "Please enter your calculations in prefix mode with spaces as separation between items."
-        print "Print q to quit."
-        user_calculating = raw_input("What would you like to calculate?")
+        user_calculating = raw_input("What would you like to calculate? ")
         tokens = user_calculating.split(" ") # ["pow", "3", "5"]
         if tokens[0] == "q":
             break
-        elif tokens[0] == "add":
-            return arithmetic1.add()
+        elif tokens[0] == "+":
+            print add(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == "-":
+            print subtract(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == "*":
+            print multiply(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == "/":
+            print divide(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == "squ":
+            print square(int(tokens[1]))
+        elif tokens[0] == "cube":
+            print cube(int(tokens[1]))
+        elif tokens[0] == "pow":
+            print power(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == "mod":
+            print mod(int(tokens[1]), int(tokens[2]))
 
 
 if __name__ == "__main__":
     calculator()
-
-
